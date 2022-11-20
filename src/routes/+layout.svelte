98bda266c7c5d71494f711b86c697e98d5d98069
Xulio Coira <xulioc@gmail.com>
2022-11-20 17:32:33 +0100

    disable right click

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.css';
 	import { PUBLIC_APP_NAME } from '$env/static/public';
 
+	/* disable right click */
+	document.addEventListener('contextmenu', (event) => event.preventDefault());
+
 	// import { online } from '$lib/stores/online';
 </script>
 
