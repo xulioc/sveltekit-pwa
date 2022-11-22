4d58fec0049d9ec2a3d3bade2d65a670e32bacee
Xulio Coira <xulioc@gmail.com>
2022-11-22 17:08:40 +0100

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
 	import '../app.css';
-	import { PUBLIC_APP_NAME } from '$env/static/public';
+	import { APP_NAME } from '$lib/constants';
 
 	/* disable right click */
 	document.addEventListener('contextmenu', (event) => event.preventDefault());
 </script>
 
 <svelte:head>
-	<title>{PUBLIC_APP_NAME}</title>
-	<meta name="description" content={PUBLIC_APP_NAME} />
+	<title>{APP_NAME}</title>
+	<meta name="description" content={APP_NAME} />
 </svelte:head>
 
 <slot />
