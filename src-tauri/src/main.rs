c47f0dc9b6b0b8b9ae3eed09ce66a9fd0c2903e8
Xulio Coira <xulioc@gmail.com>
2022-11-25 18:33:29 +0100

    wip

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,22 @@
   windows_subsystem = "windows"
 )]
 
+// https://crates.io/crates/user-idle
+use user_idle::UserIdle;
+
+// https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn user_iddle() -> String {
+  let idle = UserIdle::get_time().unwrap();
+  println!("---");
+
+  "Hello from Rust!".into()
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![user_iddle])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
